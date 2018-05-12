1dd7248ab84d352c5a676967815c4e429e909d8a
Preston Hill <Preston0705@gmail.com>
2018-05-11 20:05:39 -0700

    code clean

--- src/js/components/Summary/Summary.jsx
+++ src/js/components/Summary/Summary.jsx
@@ -13,11 +13,7 @@ function formatCurrency(amount) {
     return `$${dollars.toLocaleString()}.${cents}`;
   }
 
-  // const dollars = Math.ceil(amount);
-  // const cents = Math.floor((amount - dollars) * 100 * -1)
-  //   .toString()
-  //   .padEnd(2, "0");
-  // return `-$${(dollars * -1).toLocaleString()}.${cents}`;
+
 }
 
 class Summary extends React.Component {
